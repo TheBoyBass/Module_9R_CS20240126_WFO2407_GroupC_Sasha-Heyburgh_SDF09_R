cff5696cef8285d817a7531ac09f380130452ee6
Lindokuhle Sibeko <138812996+TheBoyBass@users.noreply.github.com>
2024-08-04 05:35:02 +0200

    Added the start game button

--- index.js
+++ index.js
@@ -6,18 +6,19 @@ let sum = firstCard + secondCard
 let message = ""
 
 // MainCode
-console.log("The sum of your cards is" ,sum) //sum output
 
+// Adding start game function   
+function startGame() {
 //settingup winning and losing conditions of the game
-if (sum <= 20) {
-    message = "You do not have Blackjack. You need to draw another card"
-    } else if (sum === 21) {
-        message = "You have Blackjack! Congratulations!"
-        } else {
-            message = "You bust! Game over!"
-            }
+    if (sum <= 20) {
+        message = "You do not have Blackjack. You need to draw another card"
+        } else if (sum === 21) {
+            message = "You have Blackjack! Congratulations!"
+            } else {
+                message = "You bust! Game over!"
+                }
             
 
-console.log(message) //message output
-
-
+console.log("The sum of your cards is" ,sum) //sum output
+console.log(message) //message output}
+}
