dc38ea0a1e6bae8d9799fa7e40cedcc4014eabae
Lindokuhle Sibeko <138812996+TheBoyBass@users.noreply.github.com>
2024-08-04 21:55:17 +0200

    Added draw new card function

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // Decelerations
 
-let firstCard = 12 // creating the first card variable and giving it a random value
-let secondCard = 10 // creating the second card variable and giving it a random value
+let firstCard = 10 // creating the first card variable and giving it a random value
+let secondCard = 5 // creating the second card variable and giving it a random value
 let sum = firstCard + secondCard // creating the sum variable and giving the addition values of card 1 and 2
 let message = "" // creating the message variable and giving it an empty string value
 let messageEl=document.getElementById("message-el") // Storing the message paragraph in a variable called messageEl
 let sumEl = document.getElementById("sum-el") // Storing the sum paragraph in a variable called sumEl
 let cardsEl = document.getElementById("card-el") // Storing the card paragraph in a variable called cardsEl
+let NewCard = 6
 
 // MainCode
 
@@ -28,6 +29,8 @@ function startGame() {
     cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
 }
 
+//New Card Function adds previous New card to Previous sum
 function newCard() {
-    
+    sum= sum + NewCard
+    startGame() //Calling startGame()
 }
