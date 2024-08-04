2e1341e83c8c4437ff441479719515ca3701eed5
Lindokuhle Sibeko <138812996+TheBoyBass@users.noreply.github.com>
2024-08-05 01:25:25 +0200

    Added GetRandom Function

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 // Decelerations
 
-let firstCard = 10 // creating the first card variable and giving it a random value
-let secondCard = 5 // creating the second card variable and giving it a random value
+let firstCard = getRandom() // creating the first card variable and giving it a random value
+let secondCard = getRandom() // creating the second card variable and giving it a random value
 let cards = [firstCard, secondCard] //Creating the cards array
 let sum = firstCard + secondCard // creating the sum variable and giving the addition values of card 1 and 2
 let message = "" // creating the message variable and giving it an empty string value
 let messageEl=document.getElementById("message-el") // Storing the message paragraph in a variable called messageEl
 let sumEl = document.getElementById("sum-el") // Storing the sum paragraph in a variable called sumEl
 let cardsEl = document.getElementById("card-el") // Storing the card paragraph in a variable called cardsEl
-let NewCard = 6
+let NewCard = getRandom()
 
 // MainCode
 
+// function to get random number between from to 11
+function getRandom() {
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
+        return 10
+        } else if (randomNumber == 1) {
+            return 11
+            } else {
+                return randomNumber
+            }
+
+            
+    
+}
 // Adding Start game function  
 function startGame() {
     renderGame()
@@ -32,6 +46,8 @@ function renderGame() {
 
     messageEl.textContent = message
     sumEl.textContent = "Sum: " + sum
+    
+    // For loop updates cards in game
     cardsEl.textContent = "Cards: " 
     for (let i = 0; i < cards.length; i++) {
         cardsEl.textContent += cards[i] + " "
