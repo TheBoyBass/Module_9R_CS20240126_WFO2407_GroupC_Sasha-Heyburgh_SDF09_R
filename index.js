a51f652ed70c4bbca69eb7b110a6474ef2d5b461
Lindokuhle Sibeko <138812996+TheBoyBass@users.noreply.github.com>
2024-08-04 21:11:31 +0200

    Added code comments

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 // Decelerations
 
-let firstCard = 12
-let secondCard = 10
-let sum = firstCard + secondCard
-let message = "" 
-let messageEl=document.getElementById("message-el")
-console.log(messageEl)
-let sumEl = document.getElementById("sum-el")
+let firstCard = 12 // creating the first card variable and giving it a random value
+let secondCard = 10 // creating the second card variable and giving it a random value
+let sum = firstCard + secondCard // creating the sum variable and giving the addition values of card 1 and 2
+let message = "" // creating the message variable and giving it an empty string value
+let messageEl=document.getElementById("message-el") // Storing the message paragraph in a variable called messageEl
+let sumEl = document.getElementById("sum-el") // Storing the sum paragraph in a variable called sumEl
 
 // MainCode
 
